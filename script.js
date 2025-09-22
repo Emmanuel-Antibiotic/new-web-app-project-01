207d47af6f9d0ef268b594932e849ce857fac131
Emmanuel-Antibiotic <projectpalng@gmail.com>
2025-09-22 17:34:37 +0100

    The Figma web app

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector(".primary").addEventListener("click", () => {alert("Booking system coming soon!");
+});
+document.querySelector(".secondary").addEventListener("click", () => {alert("Video player coming soon!");
+});
